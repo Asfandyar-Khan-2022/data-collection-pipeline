35e31650d18a29e50cd1c86404391ffca75eb532
asfandyar-khan-2022 <a_asfandiyar@live.com>
2022-11-14 06:34:06 +0000

    Added documentation to CEX_scraping

--- CEX_scraping.py
+++ CEX_scraping.py
@@ -6,14 +6,15 @@ import json
 import requests 
 import shutil 
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
 
 class crawler:
 
-
+   
     def __init__(self):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("detach", True)
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(options=options)
         self.phones_names_list = []
         self.phones_price_list = []
@@ -25,10 +26,12 @@ class crawler:
         URL = f"https://uk.webuy.com/search?stext=iphone%207%20plus"
         self.driver.get(URL)
         time.sleep(3) 
+
         try:
             accept_cookies_button = self.driver.find_element(By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
             accept_cookies_button.click()
             time.sleep(1)
+
         except:
             pass
     
@@ -38,11 +41,17 @@ class crawler:
         grade_a.click()
         grade_b = self.driver.find_element(By.XPATH, value='//*[@id="__layout"]/div/div[6]/div[2]/div[1]/div/div/div[47]/ul/li[2]')
         grade_b.click()
+
+
+    def go_into_page_and_out(self):
         in_and_out = self.driver.find_element(By.XPATH, value ='//*[@id="__layout"]/div/div[6]/div[1]/div[2]/div/div/div/div/div[3]/div[1]/div/div[2]/h1/a/span')
         in_and_out.click()
         time.sleep(3)
         self.driver.back()
         time.sleep(3)
+    
+
+    def scroll_to_bottom(self):
         previouse_height = self.driver.execute_script('return document.body.scrollHeight')
 
         while True:
@@ -57,39 +66,45 @@ class crawler:
         return True
     
 
-    def list_all_phones(self):
-        dict_phones = {'Phone': [], 'Condition': [], 'Price': []}
-        phone_name = self.driver.find_elements(By.CSS_SELECTOR, 'span.ais-Highlight')
-        we_sell_price = self.driver.find_elements(By.CSS_SELECTOR, 'div.priceTxt')
+    def gather_image_data(self):
         image_data = self.driver.find_elements(By.CSS_SELECTOR, 'img.t058-product-img')
-        new_list = []
 
         for i in image_data:
             dict_image_link = {}
             dict_image_link['Image URL'] = [i.get_attribute('src')]
             self.image_url_list.append(dict_image_link)
+    
 
+    def buying_and_selling_price(self):
+        new_list = []
+        we_sell_price = self.driver.find_elements(By.CSS_SELECTOR, 'div.priceTxt')
         for i in we_sell_price:
             new_list.append(i.text)
         composite_list = [new_list[x:x+3] for x in range(0, len(new_list),3)]
-   
-        for i in phone_name:
-            dict_phones = {'Phone': [], 'Condition': [], 'Price': [], 'Image URL': [], 'Time': []}
-            dict_phones['Phone'].append(i.text)
-            dict_phones['Condition'].append(i.text[-1])
-            self.phones_names_list.append(dict_phones)
 
         for i in composite_list:
             dict_phones_price = {}
             dict_phones_price['Price'] = i
             self.phones_price_list.append(dict_phones_price)
 
+
+    def phone_name_and_condition(self):
+        phone_name = self.driver.find_elements(By.CSS_SELECTOR, 'span.ais-Highlight')
+        for i in phone_name:
+            dict_phones = {'Phone': [], 'Condition': [], 'Price': [], 'Image URL': [], 'Time': []}
+            dict_phones['Phone'].append(i.text)
+            dict_phones['Condition'].append(i.text[-1])
+            self.phones_names_list.append(dict_phones)
+
+
+    def add_url_price_time_to_dictionary(self):
         for i in range (len(self.phones_names_list)):
             self.phones_names_list[i]['Price'] = self.phones_price_list[i]['Price']
             self.phones_names_list[i]['Image URL'] = self.image_url_list[i]['Image URL']
             now = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
             self.phones_names_list[i]['Time'] = now
     
+
     def download_img(self):    
         for i in range(len(self.image_url_list)):
             image_url = self.image_url_list[i]['Image URL'][0]
@@ -106,11 +121,19 @@ class crawler:
                 print('Image sucessfully Downloaded: ',filename)
             else:
                 print('Image Couldn\'t be retreived')
+        
 
 
 if __name__ == "__main__":
     start_crawling = crawler()
     start_crawling.load_and_accept_cookies()
-    if start_crawling.device_grade():
-        start_crawling.list_all_phones()
+    start_crawling.device_grade()
+    start_crawling.go_into_page_and_out()
+    if start_crawling.scroll_to_bottom():
+        start_crawling.gather_image_data()
+        start_crawling.buying_and_selling_price()
+        start_crawling.phone_name_and_condition()
+        start_crawling.add_url_price_time_to_dictionary()
         start_crawling.download_img()
+    start_crawling.driver.quit()
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Data Collection Pipeline
 
 So far the data collected correlates to the name of the device and the various prices for it. THe data is stored in a list. With each device having a dicitonary that stores the device name, the device condition and the device cost.
+
+The code has been updated so now it is able to extract image data and download it to the local directory. Furthermore the images are stored with a unique and trackable name.
